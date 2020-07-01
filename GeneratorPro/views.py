3ddba1eb5748ac478258ed428c1c6fdb21394305
Naveen Thurimerla <naveenreddythurimerla@gmail.com>
2020-07-02 00:31:00 +0530

    add web templates

--- GeneratorPro/views.py
+++ GeneratorPro/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+import qrcode as qr
 
-# Create your views here.
+
+
+
+def index(request):
+    return render(request, 'GeneratorPro/index.html')
+
+
+
