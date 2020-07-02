4f9a9b93aa2065cb0d807f57ae67fac7936c8f38
Naveen Thurimerla <naveenreddythurimerla@gmail.com>
2020-07-02 20:36:15 +0530

    add new text input

--- GeneratorPro/views.py
+++ GeneratorPro/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 import qrcode as qr
-
-
+from .forms import TextForm
 
 
 def index(request):
-    return render(request, 'GeneratorPro/index.html')
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = TextForm(request.POST)
 
+        # check whether it's valid:
+        if form.is_valid():
+            qr_text = qr.make("testing")
+            return HttpResponseRedirect(qr_text.show() )
 
-
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = TextForm()
+    
+    return render(request, 'GeneratorPro/index.html', {'form': form})
