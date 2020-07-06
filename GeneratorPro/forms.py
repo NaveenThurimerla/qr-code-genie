5bfad688ce1e6c2067cb046ea180e9d98315b110
Naveen Thurimerla <naveenreddythurimerla@gmail.com>
2020-07-06 06:58:18 +0530

    add wifi and url change the drop button

--- GeneratorPro/forms.py
+++ GeneratorPro/forms.py
@@ -1,9 +1,20 @@
-from django import  forms
+from django import forms
+
 
 class PlainTextForm(forms.Form):
     user_plain_text = forms.CharField(label='Enter text', max_length=100)
 
+
+class URLForm(forms.Form):
+    user_url = forms.URLField(label='Enter Valid URL', max_length=100)
+
+
 class WIFIConfigForm(forms.Form):
     wifi_config_ssid = forms.CharField(label='Enter SSID', max_length=100)
-    wifi_configpassword = forms.CharField(label='Enter Password', max_length=100)
-    wifi_configsecurity = forms.CharField(label='Enter Encryption Type', max_length=100)
+    wifi_configpassword = forms.CharField(
+        label='Enter Password', max_length=100)
+    wifi_configsecurity = forms.TypedChoiceField(
+        label="Select Encryption Type",
+        choices=(("WEP", "WEP"), ("WPA", "WPA")),
+        required=True,
+    )
--- GeneratorPro/views.py
+++ GeneratorPro/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from io import BytesIO
 import base64
-from .forms import PlainTextForm, WIFIConfigForm
+from .forms import PlainTextForm, WIFIConfigForm, URLForm
 import segno
 import qrcode
 from segno import helpers
@@ -12,21 +12,30 @@ from segno import helpers
 def index(request, *args, **kwargs):
     default_qr_image = "static/img/default-qr.jpg"
     qr_image = None
-    plain_text_form = PlainTextForm()
-    wifi_config_form = WIFIConfigForm()
 
     if request.method == 'POST':
         wifi_config_form = WIFIConfigForm(request.POST)
         plain_text_form = PlainTextForm(request.POST)
+        url_form = URLForm(request.POST)
+
         if plain_text_form.is_valid():
             qr_image = get_plain_text_qrcode(request)
         elif wifi_config_form.is_valid():
             qr_image = get_wifi_config_qrcode(request)
+        elif url_form.is_valid():
+            qr_image = get_url_qrcode(request)
 
-    return render(request, "GeneratorPro/index.html", context={
-        'plain_text_form': plain_text_form, 'wifi_config_form': wifi_config_form, 'default_qr_image': default_qr_image, 'qr_image': qr_image
+    else:
+        plain_text_form = PlainTextForm()
+        wifi_config_form = WIFIConfigForm()
+        url_form = URLForm()
+
+    context = {
+        'plain_text_form': plain_text_form, 'wifi_config_form': wifi_config_form, 'url_form': url_form, 'default_qr_image': default_qr_image, 'qr_image': qr_image
     }
-    )
+
+    return render(request, "GeneratorPro/index.html", context
+                  )
 
 
 def get_plain_text_qrcode(request, *args, **kwargs):
@@ -35,6 +44,19 @@ def get_plain_text_qrcode(request, *args, **kwargs):
         if plain_text_form.is_valid():
             image = segno.make(
                 plain_text_form.cleaned_data['user_plain_text'],  micro=False, error="h")
+            buffer = BytesIO()
+            image.save(buffer, "PNG",  scale=20, dark='black',
+                       light='white', border=10)
+            img_str = base64.b64encode(buffer.getvalue())
+            return str(img_str.decode("utf-8"))
+
+
+def get_url_qrcode(request, *args, **kwargs):
+    if request.method == 'POST':
+        url_form = URLForm(request.POST)
+        if url_form.is_valid():
+            image = segno.make(
+                url_form.cleaned_data['user_url'],  micro=False, error="h")
             buffer = BytesIO()
             image.save(buffer, "PNG",  scale=20, dark='black',
                        light='white', border=10)
